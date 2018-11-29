e34a8982258b95fd7224a9574da32e17a27d600a
zhenfeng.jiang <zhenfeng.jiang@yidatec.com>
2018-11-29 09:52:43 +0800

    config

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  css: {
+    loaderOptions: {
+      less: {
+        modifyVars: {
+          red: '#03a9f4',
+          blue: '#3eaf7c',
+          orange: '#f08d49',
+          'text-color': '#111'
+        }
+      }
+    }
+  }
+};
